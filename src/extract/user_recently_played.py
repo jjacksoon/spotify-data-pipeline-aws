993bbe4a66be4dc10e5c169c1cc5ac7b677a4f6b
jjacksoon <jsnascimento1@hotmail.com>
2025-12-19 18:40:57 -0300

    Implementação de função para buscar faixas produzidas recentemente pelo usuário da API

--- src/extract/user_recently_played.py
+++ src/extract/user_recently_played.py
@@ -1,15 +1,14 @@
 import requests
 
-
-def get_user_top_artists(access_token: str, limit: int = 20) -> dict:
+def get_recently_played(access_token: str, limit: int = 20) -> dict:
     headers = {
         "Authorization": f"Bearer {access_token}"
     }
 
     response = requests.get(
-        "https://api.spotify.com/v1/me/top/artists",
-        headers=headers,
-        params={"limit": limit}
+        "https://api.spotify.com/v1/me/player/recently-played",
+        headers =  headers,
+        params = {"limit" : limit}
     )
 
     response.raise_for_status()
--- src/pipeline.py
+++ src/pipeline.py
@@ -1,7 +1,7 @@
 import json
 import requests
 from pathlib import Path
-from extract.user_top_artists import get_user_top_artists
+from extract.user_recently_played import get_recently_played
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 TOKEN_PATH = BASE_DIR / "token.json"
@@ -11,31 +11,18 @@ def load_access_token():
     with open(TOKEN_PATH) as f:
         return json.load(f)["access_token"]
 
-
-def run():
+#Pegando músicas ouvidas recentemente
+def run_recently_played():
     token = load_access_token()
-    
-    # Chama a função que retorna os dados e o status - top 10 artistas mais ouvidos
-    data, status_code = get_user_top_artists(token, 10)
-    
+
+    data, status_code = get_recently_played(token, 10)
     try:
         if status_code == 200:
-            print(f"Requisição feita com sucesso: {status_code}\n")
-
-            for i, artist in enumerate(data["items"], start=1):
-                print(f"{i}. {artist['name']}")
-                print(f"   Popularidade: {artist['popularity']}")
-                print(f"   Seguidores: {artist['followers']['total']}")
-                print(f"   Gêneros: {', '.join(artist['genres']) if artist['genres'] else 'N/A'}")
-                print("-" * 50)
+            print(data)
         else:
-            # Se o status não for 200, lança uma exceção
             raise Exception(f"Requisição não foi bem sucedida. Status code retornado: {status_code}")
-
     except Exception as e:
-        # Captura a exceção e mostra na tela
-        print("Erro na requisição:", e)
-
+        print("Erro na requisição", e)
 
 if __name__ == "__main__":
-    run()
+    run_recently_played()
