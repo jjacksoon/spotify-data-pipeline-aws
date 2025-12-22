eca586b118166564113d5a6df09e17ad4cb120f0
jjacksoon <jsnascimento1@hotmail.com>
2025-12-22 17:15:08 -0300

    Implementação de incremental load na camada Silver

--- src/transform/silver/silver_recently_played.py
+++ src/transform/silver/silver_recently_played.py
@@ -21,7 +21,7 @@ def read_raw_files():
     for file in raw_files:
         print(f"📄 Lendo RAW: {file}")
         with open(file,"r", encoding="utf-8") as f:
-            data = json.load(f)
+            data = json.load(f)                         #lendo conteudo json
             items = data.get("items",[])
             all_items.extend(items)
 
@@ -35,7 +35,7 @@ def transform_items(items: list) -> pd.DataFrame:
         album = track.get("album", {})
         artists = track.get("artists", [])
 
-        row = {
+        rows.append({
             "played_at": item.get("played_at"),
             "track_id": track.get("id"),
             "track_name": track.get("name"),
@@ -48,9 +48,7 @@ def transform_items(items: list) -> pd.DataFrame:
             "artist_id": artists[0].get("id") if artists else None,
             "artist_name": artists[0].get("name") if artists else None,
             "load_date": datetime.now().date()
-        }
-
-        rows.append(row)
+        })
 
     df = pd.DataFrame(rows)
 
@@ -61,6 +59,9 @@ def transform_items(items: list) -> pd.DataFrame:
     if "played_at" in df.columns:
         df["played_at"] = pd.to_datetime(df["played_at"], errors="coerce")
 
+        '''coerce - se algum valor não puder ser convertido para datetime, 
+        ele será transformado em NaT (Not a Time), evitando erros de execução.'''
+
     if "album_release_date" in df.columns:
         df["album_release_date"] = pd.to_datetime(
             df["album_release_date"], errors="coerce"
@@ -69,30 +70,51 @@ def transform_items(items: list) -> pd.DataFrame:
     return df
 
 
-def save_silver(df : pd.DataFrame):
-    if df.empty:
+def save_silver_incremental(df_new: pd.DataFrame): #parametro passado: tudo da raw
+    if df_new.empty:
         print("⚠️ Nada novo para salvar na Silver")
         return
 
     if SILVER_FILE.exists():
-        df_existing = pd.read_csv(SILVER_FILE, parse_dates = ["played_at"])
-        df_final = pd.concat([df_existing, df], ignore_index = True)
-        df_final = df_final.drop_duplicates(subset = ["track_id","played_at"])
+        '''Leitura da Silver atual, que contém todos os registros salvos anteriormente.
+            parse_dates=["played_at"] garante que a coluna played_at será lida como datetime,
+            para facilitar comparações.
+        '''
+        df_existing = pd.read_csv(SILVER_FILE, parse_dates = ["played_at"]) 
+    
+        # Mantém só o que ainda não existe 
+        '''
+            Cruze df_new com df_existing usando track_id e played_at,
+            mantendo todas as linhas de df_new
+            e me diga se cada linha encontrou correspondência ou não
+        ''' 
+        df_merged = df_new.merge(
+            df_existing[["track_id","played_at"]],
+            on = ["track_id","played_at"],
+            how = "left",                   #“Tudo que está no df_new e ainda não está na Silver”
+            indicator = True
+        )
 
-        #df.to_csv(SILVER_FILE, mode = "a", index = False, header = False)
+        df_to_insert = df_merged[df_merged["_merge"] == "left_only"] #“Quero somente os registros que ainda não existem na Silver”
+        df_to_insert = df_to_insert.drop(columns="_merge") #Remove a coluna técnica _merge que só serve para controle interno
+
     else:
-        df_final = df
-        #df.to_csv(SILVER_FILE, index = False)
+        df_to_insert = df_new
 
-    df_final.to_csv(SILVER_FILE, index = False)
-    print(f"✅ Silver atualizada: {len(df_final)} registros")
+    if df_to_insert.empty:
+        print("⚠️ Silver já está atualizada — nenhum novo registro")
+        return
+
+    #Salvamento do arquivo
+    if SILVER_FILE.exists():
+        df_to_insert.to_csv(SILVER_FILE, mode="a", index=False, header=False)
+    else:
+        df_to_insert.to_csv(SILVER_FILE, index=False)
+
+    print(f"✅ Silver incrementada com {len(df_to_insert)} novos registros")
 
 def run_silver():
     items = read_raw_files()
     print(f"🔎 Total de items lidos da Bronze: {len(items)}")
     df = transform_items(items)
-    save_silver(df)
-
-'''if __name__ == "__main__":
-    run_silver()
-'''
+    save_silver_incremental(df)
